91a661fd57bd0623b8b6d24f35273b0e2bcd5dd9
nikitaskar <n.skargovskii@gmail.com>
2018-03-29 23:06:15 +0200

    first commit

---
+++ src/components/home.vue
@@ -0,0 +1,61 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'veiiiillle'
+    }
+  },
+  mounted() {
+    this.setListeners()
+    this.setUpScene()
+  },
+  methods: {
+    setUpScene() {
+      this.pixiApp = new PIXI.Application({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        antialias: true,    
+        transparent: true,
+      })
+      this.pixiApp.renderer.autoResize = true;
+      document.body.appendChild(this.pixiApp.view);
+    },
+
+    onResize() {
+      this.pixiApp.renderer.resize(window.innerWidth, window.innerHeight);
+
+    },
+
+    setListeners() {
+      window.addEventListener('resize', this.onResize)
+    }
+  },
+ 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+canvas {
+  width: 100%;
+  height: 100%;
+}
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
