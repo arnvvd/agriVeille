016447e2a653b114baa45b52afb9678314315bc6
Arnvvd <pinot.arnaud@gmail.com>
2018-04-08 19:06:06 +0200

    Add popins

--- src/01_layout/Ending.vue
+++ src/01_layout/Ending.vue
@@ -2,11 +2,13 @@
     <div class="ending">
         <div class="centered">
             <div class="ending__wrapper">
+                <img class="ending__top" :src="topImage" alt="John triste">
                 <div class="ending__content">
-                    <h2 class="ending__title">M'voilà plumé&nbsp;!</h2>
-                    <p>Vous devinerez pas ce qui vient de m’arriver, c’est l’huissier qu’a débarqué et qui m’a tout pris ! Bon faut dire que j’avais accumulé une légère somme non payée : 320 650 euros ! Eh oui, digitaliser sa ferme, c’est pas donné et t’as intérêt à faire des choix stratégiques si tu veux pas qu’la technologie te mette plus dans le fumier qu’elle ne t’en sorte ! Par exemple, quand je repense au drone, bah ça m’a pas permis d’économiser pu d’argent que je n’en ai dépensé pour cartographier les champs, hein... </p>
-                    <p>Et pis, j’dois dire qu’au bout d’un moment c’était une sacrée fatigue psychologique de s’occuper de tous ces machins connectés, et qu’ça me prenait plus la tête à regarder mon portable en permanence qu’ça m’aidait. Sans compter les pannes et incidents techniques qu’ont pu arriver sans prévenir ! Bon après, j’ai l’air très remonté mais c’est parce que je vins de tout perdre mais je dois dire que si j’avais été pu raisonnable ça m’aurait été d’une sacrée aide toute cette technologie. Déjà comme argument pour recruter des gars qui veulent pas faire le sale boulot de nettoyage des bâtiments d’élevage par exemple.</p>
-                    <p>Et pis aussi, si j’ai acheté autant de machins, c’est pace-que j’ai voulu soutenir les starts-ups françaises, pionnières dans le domaine de l’agrotech ! C’est d’ailleurs marrant qu’les gens pensent que l’agriculture, c’est pas un domaine connecté vu tout ce qui existe depuis longtemps et qui est en train de se lancer en ce moment hein ! Rapport à mon accent peut-être ? ;)</p>
+                    <div class="ending__content__inner">
+                        <h2 class="ending__title heading--4">M'voilà plumé&nbsp;!</h2>
+                        <p class="teasing--4">« Vous devinerez pas ce qui vient de m’arriver, c’est l’huissier qu’a débarqué et qui m’a tout pris&nbsp;! J’avais accumulé 320 650 euros de dette faut dire !</p>
+                        <p class="teasing--4">Eh oui, digitaliser sa ferme, c’est pas donné et t’as intérêt à faire des choix stratégiques si tu veux pas qu’la technologie te mette plus dans le fumier qu’elle ne t’en sorte&nbsp;!&nbsp;»</p>
+                    </div>
                 </div>
             </div>
         </div>
@@ -14,7 +16,15 @@
 </template>
 
 <script>
-export default {}
+import endTop from '../../static/assets/svg/endtop.svg'
+
+export default {
+    data () {
+        return {
+            topImage: endTop
+        }
+    }
+}
 </script>
 
 
@@ -28,12 +38,12 @@ export default {}
         left: 0;
         width: 100vw;
         height: 100vh;
-        background-color: rgba(#F1C08F, .8);
+        background-color: rgba(#FFF, .6);
         z-index: 99;
 
         &__wrapper {
             display: flex;
-            flex-direction: row;
+            flex-direction: column;
             justify-content: center;
             align-items: center;
             height: 100vh;
@@ -42,9 +52,44 @@ export default {}
         &__content {
             position: relative;
             max-width: 70rem;
-            background-color: $white;
-            padding: 4rem;
-            border-radius: .5rem
+            background-color: #A5A39F;
+            padding: 2.5rem;
+            border: 3px solid $black;
+            border-radius: .5rem;
+
+            &:before,
+            &:after {
+                content: '';
+                position: absolute;
+                width: 7rem;
+                height: 10rem;
+                background-size: contain;
+            }
+
+            &::before {
+                top: 5rem;
+                left: -3rem;
+                background-image: url($svgs_path + 'endleft.svg');
+            }
+
+            &:after {
+                top: 5rem;
+                right: -3rem;
+                background-image: url($svgs_path + 'endright.svg');
+            }
+
+            &__inner {
+                padding: 4rem;
+                background-color: $main-color;
+                border: 3px solid $black;
+                border-radius: .5rem;
+            }
         } 
+
+        &__top {
+            padding: 0 4rem;
+            max-width: 70rem;
+            transform: translateY(1rem);
+        }
     }
 </style>
--- src/01_layout/Onboarding.vue
+++ src/01_layout/Onboarding.vue
@@ -3,10 +3,12 @@
         <div class="centered">
             <div class="onboarding__wrapper">
                 <div class="onboarding__content">
-                    <h2 class="onboarding__title">Bien l'bonjour à toi, visiteur de l’internet&nbsp;!</h2>
-                    <p> Moi c’est John, 52 ans, originaire d’Irlande. V’là 3 ans que j’ai quitté ma bourgade natale pour v'nir m’installer en France près de Beauvais et devenir agriculteur éleveur. Y a quelques mois de ça, plusieurs de mes connaissances et amis m’ont convaincu de digitaliser ma ferme. Un sacré chamboulement&nbsp;! C’est donc pour raconter mon expérience que j’ai créé ce p'tit blog qui te montrera en détails comment tout ça s’est goupillé.</p>
-                    <p>Mais assez perdu d'temps&nbsp;! Ferme ce pop-up, scrolle pour remplir la jauge de digitalisation et clique sur les éléments qui apparaissent à l’écran pour accéder à mon journal de bord.</p>
-                    <button class="btn btn--onboarding btn--primary" v-on:click="setOnboarded">Commencer&nbsp;!</button>
+                    <div class="onboarding__content__inner">
+                        <h2 class="onboarding__title heading--4">Bien l'bonjour à toi, visiteur de l’internet&nbsp;!</h2>
+                        <p class="teasing--4">Moi c’est John, 52 ans, originaire d’Irlande. V’là 3 ans que j’ai quitté ma bourgade natale pour v'nir m’installer en France près de Beauvais et devenir agriculteur éleveur. Y a quelques mois de ça, plusieurs de mes connaissances et amis m’ont convaincu de digitaliser ma ferme. Un sacré chamboulement&nbsp;! C’est donc pour raconter mon expérience que j’ai créé ce p'tit blog qui te montrera en détails comment tout ça s’est goupillé.</p>
+                        <p class="teasing--4">Mais assez perdu d'temps&nbsp;! <b>Ferme ce pop-up, scrolle pour remplir la jauge de digitalisation et clique sur les éléments</b> qui apparaissent à l’écran pour accéder à mon journal de bord.</p>
+                        <button class="btn btn--onboarding btn--primary heading--3" v-on:click="setOnboarded">Commencer&nbsp;!</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -35,12 +37,12 @@ export default {
         left: 0;
         width: 100vw;
         height: 100vh;
-        background-color: rgba(#F1C08F, .8);
+        background-color: rgba(#FFF, .6);
         z-index: 99;
 
         &__wrapper {
             display: flex;
-            flex-direction: row;
+            flex-direction: column;
             justify-content: center;
             align-items: center;
             height: 100vh;
@@ -49,16 +51,26 @@ export default {
         &__content {
             position: relative;
             max-width: 70rem;
-            background-color: $white;
-            padding: 4rem;
-            border-radius: .5rem
+            background-color: #A5A39F;
+            padding: 2.5rem;
+            border: 3px solid $black;
+            border-radius: .5rem;
+
+            &__inner {
+                padding: 4rem;
+                background-color: $main-color;
+                border: 3px solid $black;
+                border-radius: .5rem;
+            }
         } 
     } 
 
     .btn--onboarding {
         position: absolute;
-        bottom: 0;
+        bottom: 1rem;
         left: 50%;
+        border: 3px solid $black;
+        background-color: $white;
         transform: translate(-50%, 50%);
     }
 </style>
